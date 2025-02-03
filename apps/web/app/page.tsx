4a9c56a1ae3816b0009adacf71dae53a95a50815
Nur A Alam <nuraalam376@gmail.com>
2025-02-03 15:12:14 +0600

    Turborepo initial setup

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,24 @@
+import Image, { type ImageProps } from "next/image";
+import styles from "./page.module.css";
+
+type Props = Omit<ImageProps, "src"> & {
+  srcLight: string;
+  srcDark: string;
+};
+
+const ThemeImage = (props: Props) => {
+  const { srcLight, srcDark, ...rest } = props;
+
+  return (
+    <>
+      <Image {...rest} src={srcLight} className="imgLight" />
+      <Image {...rest} src={srcDark} className="imgDark" />
+    </>
+  );
+};
+
+export default function Home() {
+  return (
+   <>Home Page</>
+  );
+}
