11a53402bd58fa44b0c46f762a6a6fe3e59becca
Nur A Alam <nuraalam376@gmail.com>
2025-02-11 18:52:09 +0600

    Title Metadata

--- apps/web/app/about/page.tsx
+++ apps/web/app/about/page.tsx
@@ -1,7 +1,9 @@
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-    title: "About Us"
+    title: {
+        absolute: "About"
+    }
 };
 
 export default function AboutPage() {
---
+++ apps/web/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Blog"
+};
+
+
+export default function BlogPage() {
+    return <h1>Blog</h1>
+}
